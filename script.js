de3cb0a0f2e86b866f0834b60d0d99c0684171bb
Mutia Safari <mutiasafari97@gmail.com>
2018-07-24 08:04:06 +0700

    fisrt commit

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+				$('#foto-foto1').click(function(){
+					$('#rician1').fadeToggle("3000");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto2').click(function(){
+					$('#rician2').fadeToggle("3000");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto3').click(function(){
+					$('#rician3').fadeToggle("3000");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto4').click(function(){
+					$('#rician4').fadeToggle("3000");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto5').click(function(){
+					$('#rician5').slideToggle("300");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto6').click(function(){
+					$('#rician6').slideToggle("300");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto7').click(function(){
+					$('#rician7').slideToggle("300");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto8').click(function(){
+					$('#rician8').slideToggle("300");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto9').click(function(){
+					$('#rician9').toggle("explode");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto10').click(function(){
+					$('#rician10').toggle("explode");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto11').click(function(){
+					$('#rician11').toggle("explode");
+				});
+			});
+
+			$(document).ready(function(){
+				$('#foto-foto12').click(function(){
+					$('#rician12').toggle("explode");
+				});
+			});
+
